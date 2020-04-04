0fd64c6ca13da8b6358f375370fd8ec93dd9ad11
meowrial <ali.adnani@hotmail.com>
2020-04-05 01:22:53 +0800

    generate GIF

--- generateBGs.py
+++ generateBGs.py
@@ -1,5 +1,6 @@
 import cv2
 import random
+import ffmpeg
 # Globals
 
 video_path = '[Erai-raws] Kimetsu no Yaiba - 19 [1080p][Multiple Subtitle].mkv'
@@ -25,7 +26,7 @@ def generate_clip(video_path,start_frame=2000,duration=5):
 
 
   video = cv2.VideoCapture(video_path)
-  clip_length = fps * duration
+  clip_length = int(fps * duration)
   frames_array = []
 
   video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
@@ -44,7 +45,7 @@ def write_images():
 
 def write_mp4(frames_array):
   length, width, height, fps = get_video_properties(video_path)
-  out = cv2.VideoWriter('test.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height))
+  out = cv2.VideoWriter('genGIF.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height))
   for frame in frames_array:
     out.write(frame)
 
@@ -58,7 +59,16 @@ def generate_start_frame(video_path):
   random_number = random.randrange(0,length)
   return random_number
 
+def generate_GIF():
+  stream = ffmpeg.input('genGIF.avi')
+  # stream = ffmpeg.overwrite_output(stream)
+  stream = ffmpeg.output(stream, 'genLoop.gif',s='848x480',y=None)
+  # stream = ffmpeg.overwrite_output(stream, 'genLoop.gif',s='640x480')
+
+  ffmpeg.run(stream)
+
 
 number = generate_start_frame(video_path)
-frames = generate_clip(video_path,start_frame=number, duration=5)
+frames = generate_clip(video_path,start_frame=number, duration=4)
 write_mp4(frames)
+generate_GIF()
